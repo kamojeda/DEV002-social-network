f2cb09e6b60460a46d43836b11417e132b752b25
Kamila Ojeda <beau.ojeda@gmail.com>
2023-02-08 20:53:33 -0500

    Agrega mostrar errores de validación de email y contraseña en el Home.js.

--- src/components/home.js
+++ src/components/home.js
@@ -3,9 +3,6 @@ import { auth, provider } from "../Firebase/firebase.js";
 import { signInWithPass, viewer, popUpGoogle } from "../Firebase/auth-func.js";
 
 export const home = () => {
-	//Creamos elementos del Formulario
-	//const containerHeader = document.createElement("header");
-	//const imgHeader = document.createElement("img");
 	const homeDiv = document.createElement("div");
 	const containerHeader = document.createElement("header");
 	const imgLogo = document.createElement("img");
@@ -13,8 +10,10 @@ export const home = () => {
 	const loginForm = document.createElement("form");
 	const labelMail = document.createElement("label");
 	const inputMail = document.createElement("input");
+	const paragraphMailError = document.createElement("p");
 	const labelPassword = document.createElement("label");
 	const inputPassword = document.createElement("input");
+	const paragraphPassError = document.createElement("p");
 	const buttonLogin = document.createElement("button");
 	const containerGoogle = document.createElement("div");
 	const iconLogoGoogle = document.createElement("i");
@@ -25,7 +24,6 @@ export const home = () => {
 
 	homeDiv.className = "div-container";
 	containerHeader.className = "container-header";
-	//imgHeader.className = "img-header";
 	imgLogo.src = "../img/Logo VeganShip.png";
 	imgLogo.className = "img-logo";
 	containerForm.className = "container-form";
@@ -37,14 +35,19 @@ export const home = () => {
 	inputMail.className = "input-login-mail inputs";
 	inputMail.placeholder = "tucorreo@gmail.com";
 	inputMail.required = "true";
+	paragraphMailError.className = "paragraph-mail-error hide-error-message";
+	// paragraphMailError.textContent = "Por favor, ingresa un correo";
 	labelPassword.className = "label-pass labels";
 	labelPassword.textContent = "Contraseña";
 	inputPassword.type = "text";
 	inputPassword.id = "i-input-login-password";
 	inputPassword.className = "input-login-password inputs";
-	inputPassword.placeholder = "xxxxxxxxxxxxxx";
+	inputPassword.placeholder = "***************";
 	inputPassword.type = "password";
 	inputPassword.required = "true";
+	paragraphPassError.className = "paragraph-pass-error hide-error-message";
+	// paragraphPassError.textContent = "Por favor, ingresa una contraseña";
+	// paragraphPassError.style = "display: none";
 	buttonLogin.textContent = "Iniciar sesión";
 	buttonLogin.className = "button-login buttons";
 	containerGoogle.className = "container-google";
@@ -58,15 +61,16 @@ export const home = () => {
 	hrefRegister.textContent = "Regístrate";
 	hrefRegister.className = "href-register";
 
-	//homeDiv.appendChild(loginForm);
 	homeDiv.appendChild(containerHeader);
 	containerHeader.appendChild(imgLogo);
 	homeDiv.appendChild(containerForm);
 	containerForm.appendChild(loginForm);
 	loginForm.appendChild(labelMail);
 	loginForm.appendChild(inputMail);
+	loginForm.appendChild(paragraphMailError);
 	loginForm.appendChild(labelPassword);
 	loginForm.appendChild(inputPassword);
+	loginForm.appendChild(paragraphPassError);
 	loginForm.appendChild(buttonLogin);
 	homeDiv.appendChild(containerGoogle);
 	containerGoogle.appendChild(buttonGoogle);
@@ -82,22 +86,43 @@ export const home = () => {
 			e.preventDefault();
 			const emailLogin = inputMail.value;
 			const passwordLogin = inputPassword.value;
-			console.log(emailLogin, passwordLogin);
+
 			try {
 				const userCredentials = await signInWithPass(
 					auth,
 					emailLogin,
 					passwordLogin
 				);
-				//console.log(userCredentials.user);
+				console.log(userCredentials);
 				toNavigate("/feed");
 			} catch (error) {
-				if (error.code === "auth/user-not-found") {
-					alert("usuario NO encontrado");
+				console.log(error.code);
+				if (error.code === "auth/empty-email") {
+					paragraphMailError.textContent = "Ingrese un correo";
+					paragraphMailError.style = "display: block";
+				} else if (error.code === "auth/invalid-email") {
+					paragraphMailError.textContent = "Ingrese un correo válido";
+					paragraphMailError.style = "display: block";
+				} else if (error.code === "auth/user-not-found") {
+					paragraphMailError.textContent =
+						"El correo es incorrecto o no está registrado. Puedes registrarte ahora.";
+					paragraphMailError.style = "display: block";
+				} else {
+					paragraphMailError.style = "display: none";
+				}
+
+				if (error.code === "auth/empty-password") {
+					paragraphPassError.textContent = "Ingresa contraseña";
+					paragraphPassError.style = "display: none";
 				} else if (error.code === "auth/wrong-password") {
-					alert("Contraseña incorrecta");
-				} else if (error.code) {
-					console.log(error.code);
+					paragraphPassError.textContent = "La contraseña es incorrecta";
+					paragraphPassError.style = "display: block";
+				} else if (error.code === "auth/weak-password") {
+					paragraphPassError.textContent =
+						"La contraseña debe tener más de 6 caracteres";
+					paragraphPassError.style = "display: none";
+				} else {
+					paragraphPassError.style = "display: none";
 				}
 			}
 		});
